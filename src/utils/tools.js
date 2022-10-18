2761eab72cf44e23cc12953440029039731d2ea5
tongchengbin <58296672@qq.com>
2022-10-18 19:28:14 +0800

    build

---
+++ src/utils/tools.js
@@ -0,0 +1,10 @@
+function formatSeconds(value) {
+    let result = parseInt(value)
+    let h = Math.floor(result / 3600) < 10 ? '0' + Math.floor(result / 3600) : Math.floor(result / 3600)
+    let m = Math.floor((result / 60 % 60)) < 10 ? '0' + Math.floor((result / 60 % 60)) : Math.floor((result / 60 % 60))
+    let s = Math.floor((result % 60)) < 10 ? '0' + Math.floor((result % 60)) : Math.floor((result % 60))
+    result = `${h}:${m}:${s}`
+    return result
+}
+
+export default formatSeconds
--- src/views/match/index.vue
+++ src/views/match/index.vue
@@ -43,7 +43,8 @@
         </div>
       </div>
     </div>
-    <el-dialog v-model="dialogPopVisible" center :show-close="false" custom-class="challenge-pop" :before-close="handlerClose">
+    <el-dialog v-model="dialogPopVisible" center :show-close="false" custom-class="challenge-pop"
+               :before-close="handlerClose">
       <h1 style="margin: auto;text-align: center;color: #0080ff">
         {{ detail.name }}
       </h1>
@@ -54,36 +55,44 @@
       </div>
       <div style="text-align: center;">
         <div class="blood" v-if="detail.first_blood">
-          <span   class="rank-blood">
+          <span class="rank-blood">
             <img :title="'第一滴血：'+detail.first_blood" src="../../assets/冠军.svg">&nbsp;{{ detail.first_blood }}
           </span>
         </div>
         <div class="blood" v-if="detail.second_blood">
-          <span class="rank-blood" >
+          <span class="rank-blood">
             <img :title="'第二滴血：'+ detail.second_blood" src="../../assets/亚军.svg">&nbsp;{{ detail.second_blood }}
           </span>
         </div>
         <div class="blood" v-if="detail.thrid_blood">
           <span class="rank-blood">
-            <img :title="'第三滴血：'+ detail.thrid_blood" src="../../assets/季军.svg">&nbsp;{{ detail.thrid_blood }}</span>
+            <img :title="'第三滴血：'+ detail.thrid_blood" src="../../assets/季军.svg">&nbsp;{{
+              detail.thrid_blood
+            }}</span>
         </div>
       </div>
       <div class="desc">
         <p>{{ detail.desc }}</p>
       </div>
       <div class="attachment">
-        <a v-for="item in detail.attachment" :href="item.url" target="_blank" ><el-tag effect="dark" size="mini">{{item.name}}</el-tag></a>
+        <a v-for="item in detail.attachment" :href="item.url" target="_blank">
+          <el-tag effect="dark" size="mini">{{ item.name }}</el-tag>
+        </a>
       </div>
       <div class="startContainer" v-if="detail.active_flag">
-        <el-progress :text-inside="true" v-if="detail.container" :stroke-width="20" :percentage="percentage" color="#c6e2ff"
+        <el-progress :text-inside="true" v-if="detail.container" :stroke-width="20" :percentage="percentage"
+                     color="#c6e2ff"
                      :duration="0"
-                     :format="format"  class="process">
-          <span style="color: #66b1ff;font-weight: 700">题目有效时间</span>
+                     :format="format" class="process">
+          <span style="color: #254070;font-weight: bold">{{ showText }}</span>
         </el-progress>
+        <div class="url-box" v-if="detail.container">
+          <template v-for="u in detail.container.urls">
+            <a target="_blank" :href="u.url">Origin Port:{{ u.origin }}</a>
+          </template>
+        </div>
         <el-button v-if="detail.active_flag && detail.container===null" size="mini" type="primary"
                    @click="startContainer">启动
-        </el-button>
-        <el-button v-if="detail.active_flag && detail.container != null" size="mini" type="primary" @click="openUrl">打开目标
         </el-button>
         <el-button v-if="detail.active_flag && detail.container != null" size="mini" type="primary" @click="delayed">延时
         </el-button>
@@ -104,6 +113,7 @@
 <script>
 import {ref} from "@vue/reactivity";
 import request from "@/utils/request";
+import formatSeconds from "@/utils/tools"
 import {ElMessage} from "element-plus";
 
 
@@ -126,6 +136,7 @@ export default {
     const duration = ref(0)
     const percentage = ref(0)
     return {
+      showText: "题目倒计时",
       percentage,
       duration,
       flag,
@@ -145,31 +156,37 @@ export default {
         this.challenges = res.data
       })
     },
-    handlerClose(done){
+    handlerClose(done) {
       clearTimeout(this.func)
       done()
     },
     fetchDetail(id) {
+      this.percentage = 0
       let that = this
+      clearTimeout(that.func)
       request.get(`/api/challenge/${id}`).then(res => {
         this.detail = res.data
         this.dialogPopVisible = true
         if (res.data.container !== null) {
-          let start_time = new Date(res.data.container.create_time).getTime()
-          let end_time = new Date(res.data.container.destroy_time).getTime()
-          // 开启一个计时器
+          // 这里不能用本地时间计算
+          let {create_time, timeout} = res.data.container
+          let start_time = new Date(create_time).getTime()
           that.func = setInterval(() => {
             // 获取当前时间
             let now = new Date().getTime()
-            let percentage = (now - start_time) / (end_time - start_time)
+            // 已消耗时间
+            let used = (now - start_time) / 1000
+            this.showText = `剩余${timeout-used}秒`;
+            let percentage = used/timeout
             this.percentage = (percentage > 1 ? 1 : percentage) * 100
+            console.log(this.percentage)
             if (percentage > 1) {
               clearTimeout(that.func)
               this.fetchDetail(id)
             }
-          }, 3000)
+          }, 1000)
         }
-      }).catch(err=>{
+      }).catch(err => {
 
       })
     },
@@ -182,6 +199,9 @@ export default {
       this.fetchDetail(item.id)
     },
     startContainer() {
+      // 清除定时器
+      clearTimeout(this.func)
+      this.percentage = 0;
       let id = this.detail.id;
       request.post(`/api/challenge/${id}/start`).then(res => {
         this.fetchDetail(id)
@@ -191,8 +211,8 @@ export default {
       let id = this.detail.id;
       request.post(`/api/challenge/${id}/delayed`).then(res => {
         ElMessage({
-          type:"success",
-          message:"延迟成功！"
+          type: "success",
+          message: "延迟成功！"
         })
         this.fetchDetail(id)
 
@@ -202,9 +222,10 @@ export default {
       let id = this.detail.id
       request.post(`/api/challenge/${id}/destroy`).then(res => {
         this.fetchDetail(id)
+        clearTimeout(this.func)
       })
     },
-    openUrl(){
+    openUrl() {
       let url = this.detail.container.url;
       window.open(url)
 
@@ -223,12 +244,23 @@ export default {
         })
         this.flag = ''
         this.fetchList()
+      }).catch(err=>{
+        this.fetchDetail(this.detail.id)
       })
     }
   }
 }
 </script>
 <style scoped lang="less">
+.url-box {
+  margin: 10px;
+  padding: 10px;
+
+  a {
+    text-decoration-color: #0080ff;
+    color: #fff;
+  }
+}
 
 .types {
   margin: 20px 10px;
@@ -301,7 +333,7 @@ export default {
     text-align: center;
     font-size: 20px;
     font-weight: 700;
-    color: #ffc600;
+    color: #0081ff;
     height: 30px;
     line-height: 30px;
   }
@@ -377,9 +409,11 @@ export default {
     margin-bottom: 10px;
     padding: 10px 20px;
   }
-  .attachment{
+
+  .attachment {
     margin: 10px;
   }
+
   .submit {
     text-align: center;
 
@@ -418,14 +452,17 @@ export default {
     }
   }
 }
-/deep/ .el-progress-bar__inner{
-  background-color: #ffc600!important;
+
+/deep/ .el-progress-bar__inner {
+  background-color: #66b1ff !important;
 }
+
 .dialog /deep/ .el-dialog__wrapper {
-  background-color:rgba(0,0,0,0.8);
+  background-color: rgba(0, 0, 0, 0.8);
 }
+
 /deep/ .el-empty__image {
-  polygon,rect,g,path{
+  polygon, rect, g, path {
     fill: #66b1ff;
   }
 }
