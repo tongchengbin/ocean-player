c8b9ebf0b78c23b53270a3dba8b9947693f325bb
tongchengbin <58296672@qq.com>
2022-11-26 15:59:27 +0800

    修改超时时间

--- src/utils/request.js
+++ src/utils/request.js
@@ -9,7 +9,7 @@ import router from "../router";
 const service = axios.create({
     baseURL: process.env.VUE_APP_BASE_API, // url = base url + request url
     // withCredentials: true, // send cookies when cross-domain requests
-    timeout: 10000 // request timeout
+    timeout: 60000 // request timeout
 })
 
 // request interceptor
